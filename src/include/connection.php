592b07bd5ff07f1eaa900b56c6fc5fc46d7de76e
David Lancioni <dlancioni@gmail.com>
2020-01-06 23:38:21 -0200

    Fix connection

--- src/include/connection.php
+++ src/include/connection.php
@@ -1,15 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "app";
-$password = "app";
+$servername = "sql208.unaux.com";
+$username = "unaux_25020096";
+$password = "bt7vzg1esoo";
 
 // Create connection
-echo $servername;
 $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+echo "Connected successfully to $servername";
 ?>

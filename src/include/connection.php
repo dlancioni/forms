b14d583335b736c63d5429e2ee268fb8462536f0
David Lancioni <dlancioni@gmail.com>
2020-01-08 22:15:55 -0300

    Fix UTF8 on db connection

--- src/include/connection.php
+++ src/include/connection.php
@@ -6,6 +6,7 @@
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn->set_charset("utf8");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }

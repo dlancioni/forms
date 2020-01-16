3571275a919cd3ca4ab34ab4b0509b1f041301d0
David Lancioni <dlancioni@gmail.com>
2020-01-16 10:32:54 -0300

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!-- BEGIN HTML -->
+<html>
+<body>
+<form id='Report' method='Post' action='form.php'>
+    <!-- HEADER -->
+    <?php include "web/header.php";?>
+    <?php include "src/header.php";?>
+    <?php include "menu.php";?>
+    <!-- CONTENTS -->
+    <div class="w3-row">
+        <div class="w3-col l2">&nbsp;</div>
+        <div class="w3-col l8">
+        <a href="report.php">Report</a>
+        </div>
+        <div class="w3-col l2">&nbsp;</div>
+    </div>
+    <!-- JS -->
+    <?php include "report_js.php";?>
+    <!-- FOOTER -->
+    <?php include "web/footer.php";?>
+    <?php include "web/footer.php";?>
+<!-- END HTML -->
+</form>
+</body>
+</html>

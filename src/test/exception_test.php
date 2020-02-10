b406f660474fd522e3d4652723bc12f45e9d4901
David Lancioni <dlancioni@gmail.com>
2020-02-10 00:30:32 -0300

    Enable exceptions

--- src/test/exception_test.php
+++ src/test/exception_test.php
@@ -1,18 +1,17 @@
+<?php include "../include/exception.php";?>
 <?php include "../include/connection.php";?>
 <?php include "../classes/base.php";?>
 <?php include "../classes/crud.php";?>
 <?php include "../classes/system.php";?>
 
 <?php
-    
-    try{
-        //division by zero
-        $error = 'Always throw this error';
-        throw new Exception($error);
-    }
-    catch(Exception $ex){
-        echo 'Got it!';
-    }    
 
+
+try {
+    $foo = 1/0;
+} catch (Exception $e) {
+    //var_dump($e->getTrace());
+    echo $e->getMessage();
+}
 
 ?>

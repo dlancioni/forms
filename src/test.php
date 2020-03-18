01eb91a573c52de6e3cc442e2c11038f0e0018f4
David Lancioni <dlancioni@gmail.com>
2020-03-18 16:04:29 -0300

    Working query

---
+++ src/test.php
@@ -0,0 +1,10 @@
+<?php
+
+$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+
+
+echo json_decode($json, true);
+
+
+
+?>
--- web/grid.php
+++ web/grid.php
@@ -1,5 +1,20 @@
 <?php 
-    //$columns = $rs_data->fetch_fields();    
+    try {
+
+        // Get data
+        $input = '{"session":{"id_system":1,"id_table":2,"id_action":1},"filter":[{"field_name":"id", "operator":"=", "field_value":"1"}]}';
+        $output = pg_query_params($conn, 'call query($1)', array($input));
+        while ($row = pg_fetch_row($output)) {
+            if ($row[0]) {
+                $rs = json_decode($row[0])->resultset;
+            }
+        }
+        echo $rs[0]->name;
+    } catch (exception $e) {
+        echo "deu pau";
+            echo $e;
+    }
+
 ?>
 
 <div class="w3-responsive">
@@ -33,9 +48,13 @@
                 echo "</tr>";
             }
             */
+
+
+
+
+
+
         ?>
         </tbody>
     </table>
 </div>
-
-I am a table :)

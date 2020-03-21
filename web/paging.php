f32a732c0fbf34314c3508b2da721044368945e2
David Lancioni <dlancioni@gmail.com>
2020-03-21 09:54:16 -0300

    Fix system screen

--- web/paging.php
+++ web/paging.php
@@ -1,7 +1,10 @@
+<br>
+<center>
 <?php
     if ($data) {
         $count = $data[0]->record_count;
-        if ($count > 0) {
+        // 1 page, do not show
+        if ($count > PAGE_SIZE) {
             $pages = round($count / PAGE_SIZE, 2);
             for ($i=0;$i<+$pages;$i++) {
                 $page = $i+1;
@@ -11,3 +14,4 @@
         }
     }
 ?>
+</center>

50f1fefe863307de37be96821eaa7cbb1972567a
David Lancioni <dlancioni@gmail.com>
2020-03-22 18:35:48 -0300

    Menu for form and report

--- web/menu.php
+++ web/menu.php
@@ -1,13 +1,26 @@
 </p>
 <div class="w3-bar w3-black">
   <a href="#" class="w3-bar-item w3-button w3-mobile w3-black">Home</a>
+
   <div class="w3-dropdown-hover w3-mobile">
-    <button class="w3-button">Entry<i class="fa fa-caret-down"></i></button>
+    <button class="w3-button">Report<i class="fa fa-caret-down"></i></button>
+    <div class="w3-dropdown-content w3-bar-block w3-dark-grey">
+      <a href="index.php?id_layout=1&id_table=1&page_offset=0" class="w3-bar-item w3-button w3-mobile">System</a>
+      <a href="index.php?id_layout=1&id_table=2&page_offset=0" class="w3-bar-item w3-button w3-mobile">Table</a>
+      <a href="index.php?id_layout=1&id_table=3&page_offset=0" class="w3-bar-item w3-button w3-mobile">Field</a>
+    </div>
+  </div>
+
+  <div class="w3-dropdown-hover w3-mobile">
+    <button class="w3-button">Form<i class="fa fa-caret-down"></i></button>
     <div class="w3-dropdown-content w3-bar-block w3-dark-grey">
       <a href="index.php?id_layout=2&id_table=1&page_offset=0" class="w3-bar-item w3-button w3-mobile">System</a>
       <a href="index.php?id_layout=2&id_table=2&page_offset=0" class="w3-bar-item w3-button w3-mobile">Table</a>
       <a href="index.php?id_layout=2&id_table=3&page_offset=0" class="w3-bar-item w3-button w3-mobile">Field</a>
     </div>
   </div>
+
+
+
 </div>
 

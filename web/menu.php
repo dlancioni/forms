7a63bc00c1f3261e01767df738f35d068e6abffa
David Lancioni <dlancioni@gmail.com>
2020-04-10 20:06:16 -0300

    Add cool menu with submenu

--- web/menu.php
+++ web/menu.php
@@ -1,18 +1,54 @@
 </p>
-<div class="w3-bar w3-black">
-  <a href="/php/index.php" class="w3-bar-item w3-button w3-mobile w3-black">Home</a>
 
-  <div class="w3-dropdown-hover w3-mobile">
-    <button class="w3-button">Report<i class="fa fa-caret-down"></i></button>
-    <div class="w3-dropdown-content w3-bar-block w3-dark-grey">
-      <a class="w3-bar-item w3-button w3-mobile" onClick="go(1,1,0)">System</a>
-      <a class="w3-bar-item w3-button w3-mobile" onClick="go(1,2,0)">Table</a>
-      <a class="w3-bar-item w3-button w3-mobile" onClick="go(1,3,0)">Field</a>
-      <a class="w3-bar-item w3-button w3-mobile" onClick="go(1,4,0)">Domain</a>
-      <a class="w3-bar-item w3-button w3-mobile" onClick="go(1,5,0)">Event</a>
-      <a class="w3-bar-item w3-button w3-mobile" onClick="go(1,6,0)">Code</a>
-    </div>
-  </div>
+<link rel="stylesheet" href="menu.css">
 
+<div class="menu-container">
+
+    <ul class="menu clearfix">    
+
+        <li>
+            <a href="/php/index.php" class="w3-bar-item w3-button w3-mobile"><b>Home</b></a>    
+            <ul class="sub-menu clearfix">
+                <!-- HELP MENU -->              
+            </ul>      
+        </li>
+
+
+        <li>
+            <a class="w3-bar-item w3-button w3-mobile">Entry</a>    
+            <ul class="sub-menu clearfix">
+                <!-- SETUP MENU -->
+                <li>
+                    <a href="#" class="w3-bar-item w3-button w3-mobile">Setup</a>        
+                    <ul class="sub-menu">
+                        <li>              
+                        <a onClick="go(1,1,0)" class="w3-bar-item w3-button w3-mobile">System</a>
+                        <a onClick="go(1,2,0)" class="w3-bar-item w3-button w3-mobile">Table</a>              
+                        <a onClick="go(1,3,0)" class="w3-bar-item w3-button w3-mobile">Field</a>              
+                        <a onClick="go(1,4,0)" class="w3-bar-item w3-button w3-mobile">Domain</a>              
+                        <a onClick="go(1,5,0)" class="w3-bar-item w3-button w3-mobile">Event</a>              
+                        <a onClick="go(1,6,0)" class="w3-bar-item w3-button w3-mobile">Code</a>                         
+                        </li>
+                    </ul>        
+                </li>
+                <!-- SETUP MENU -->                
+
+                <!-- HELP MENU -->
+                <li>
+                    <a href="#" class="w3-bar-item w3-button w3-mobile">Help</a>        
+                    <ul class="sub-menu">
+                        <li>              
+                        <a href="#" class="w3-bar-item w3-button w3-mobile">About</a>
+                        </li>
+                    </ul>        
+                </li>
+                <!-- HELP MENU -->
+
+            </ul>      
+        </li>
+    </ul>
 </div>
 
+
+</body>
+</html>
